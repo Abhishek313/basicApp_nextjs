646c22caf4876dd7a9918dd7d8be42fb1e136bd0
Abhishek313 <vabhi5149@gmail.com>
2024-02-24 15:07:50 +0530

    featuredSection and HeroSection completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
-   <h1 className="text-2xl text-center">Chai aur code</h1>
+  <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+   
+   <HeroSection/>
+   <FeaturedCourses/>
+  </main>
   );
 }
